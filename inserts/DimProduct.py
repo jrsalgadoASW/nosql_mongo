72361d667d3221682202404e2a9af70f51e6b4ee
Jorge Salgado <jrsalgado@asesoftware.com>
2023-03-03 16:09:45 -0500

    DimProduct insertion

---
+++ inserts/DimProduct.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB server
+client = MongoClient("mongodb://localhost:27017/")
+database = client["Customs"]
+data_semilla = database.get_collection("DataSemilla")
+pipeline = [
+    {
+        # Doc id = combination of countrycod and countryname, so they are unique.
+        "$group": {
+            "_id": {
+                "cod_item": "$COD_ITEM",
+                "item": "$ITEM",
+                "tipo_item": "$TIPO_ITEM",
+                "unidad_medida": "$UNIDAD_MEDIDA",
+            }
+        }
+    },
+    {
+        #Project only relevant fields
+        "$project": {
+            "_id":0,
+            "Cod_item": "$_id.cod_item",
+            "Item": "$_id.item",
+            "Tipo_item": "$_id.tipo_item",
+            "Unidad_medida": "$_id.unidad_medida",
+        }
+    },
+]
+
+
+dim_product = database.get_collection("DimProduct")
+dim_product.insert_many(data_semilla.aggregate(pipeline=pipeline))
+
+
+client.close()
